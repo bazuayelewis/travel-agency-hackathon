30c7e62f7a200b4aba7892c06534d7c69755c56d
Lewis Bazuaye <bazuayelewis@gmail.com>
2024-07-21 11:05:51 -0400

    updated main script

--- main.py
+++ main.py
@@ -1,16 +1,14 @@
-from utils.config import BASE_URL, ENDPOINT
+from utils.config import BASE_URL, ENDPOINT, TABLE_ID
 from utils.api_manager import get_data
 from utils.data_extractor import extract_countries_data
+from utils.bq_manager import load_data_to_bigquery
 
 def main():
     url = f"{BASE_URL}/{ENDPOINT}"
     countries_data = get_data(url)
-
     extracted_data = extract_countries_data(countries_data)
-
+    load_data_to_bigquery(extracted_data, TABLE_ID)
     return extracted_data
 
 if __name__ == "__main__":
     extracted_data = main()
-    print(len(extracted_data))
-    print(extracted_data)
