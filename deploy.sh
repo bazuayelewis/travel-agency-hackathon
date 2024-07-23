679affa5767d4cd27702a4a4756c08fede4fc2c9
Lewis Bazuaye <107050974+bazuayelewis@users.noreply.github.com>
2024-07-22 20:58:27 -0400

    Update deploy.sh
    - slight modification

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,7 @@ gcloud functions deploy dec-agency-function \
     --gen2 \
     --region europe-west1 \
     --runtime python310 \
-    --trigger-resource dec-travel-agency-test \
+    --trigger-resource dec-travel-agency \
     --trigger-event google.cloud.storage.object.v1.finalized \
     --entry-point load_to_bigquery \
     --source .
